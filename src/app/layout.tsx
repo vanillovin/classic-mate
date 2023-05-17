18676f6545fa75f30c8b8ecee374809aa105caf5
vanillovin <vanillovin@gmail.com>
2023-05-17 11:24:44 +0900

    feat: RootLayout에 Layout 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import MainNavigation from '@/components/layout/MainNavigation'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Layout from '@/components/layout/Layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Layout>
+          {children}
+        </Layout>
+      </body>
     </html>
   )
 }
