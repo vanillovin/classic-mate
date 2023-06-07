b756bff026b6d90aab852dff52697cb564415616
vanillovin <vanillovin@gmail.com>
2023-06-07 21:58:21 +0900

    refactor: ClassicItem 컴포넌트 변경

--- src/components/classics/ClassicItem.tsx
+++ src/components/classics/ClassicItem.tsx
@@ -1,25 +1,37 @@
+'use client';
+
 import Image from 'next/image';
-
 import Card from '../ui/Card';
-import { Classic } from '@/app/types/classics';
+import { type ClassicT } from '@/app/classics/types';
+import Link from 'next/link';
 
-interface Props {
-  classic: Classic
-}
 
-function ClassicItem({ classic }: Props) {
+function ClassicItem({ classic }: { classic: ClassicT }) {
   return (
-    <Card>
-      <h1 className='font-medium'>{classic.title}</h1>
-      <p>{classic.description}</p>
-      <ul className='flex items-center'>
-        {classic.tags.map((tag: string) => (
-          <li key={tag} className='rounded-sm p-1 bg-rose-600 text-white mr-1'>{tag}</li>
-        ))}
-      </ul>
-      <Image src={classic.coverImage} alt={`${classic.title} cover`} className='w-full' width={400} height={300} />
-      {/* <Link href={`classics/${classic.id}`}>더 보기</Link> */}
-    </Card>
+    <Link
+      href={`/classics/${classic.id}`}
+      className='rounded-md shadow-md p-2 flex flex-col hover:shadow-lg justify-between bg-white bg-opacity-90 transition-all hover:bg-amber-400 hover:bg-opacity-20'
+    >
+      <div>
+        <h2 className='font-semibold'>{classic.title}</h2>
+        <p className='text-sm my-1'>{classic.description}</p>
+        <ul className='flex items-center flex-wrap mb-1'>
+          {classic.tags.map((tag: string) => (
+            <li key={tag} className='my-1 text-sm rounded-sm p-1 bg-rose-600 text-white mr-1'>
+              {tag}
+              {/* <Link href={`/classics/${tag}`}>{tag}</Link> */}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='relative rounded-sm overflow-hidden h-36'>
+        <Image
+          src={classic.coverImage}
+          alt={`${classic.title} cover`}
+          fill={true}
+        />
+      </div>
+    </Link>
   )
 }
 
