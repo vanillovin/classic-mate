7c498eea8b3a5026015d8e29ac8a475d49fecd9b
vanillovin <vanillovin@gmail.com>
2023-06-10 20:10:07 +0900

    remove

--- src/app/classics/api.ts
+++
@@ -1,6 +0,0 @@
-import { allClassics } from '@/data/allClassics';
-import { type ClassicT } from "./types";
-
-export async function getAllClassics(): Promise<ClassicT[]> {
-  return allClassics; 
-}
