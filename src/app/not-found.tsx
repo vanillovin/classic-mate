4e00e91ed6e74909710b1dcb7cf615c505aa3521
vanillovin <vanillovin@gmail.com>
2023-06-10 19:41:40 +0900

    refactor: not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function NotFoundPage() {
-  return <h1>페이지를 찾을 수 없습니다</h1>;
+  return (
+    <div className='text-center mt-20'>
+			<h1 className='text-2xl mb-5'>페이지를 찾을 수 없습니다</h1>
+      <Link href="/" className='text-end border-b pl-4 border-black p-1 font-medium hover:text-yellow-500 transition-all'>
+        홈으로 돌아가기
+      </Link>
+		</div>
+  )
 }
