35d56943ee1fb0eecb11209d9ed764e53f3932ca
vanillovin <vanillovin@gmail.com>
2023-06-10 15:19:16 +0900

    feat: createServerClient -> createClient

--- src/app/classics/page.tsx
+++ src/app/classics/page.tsx
@@ -1,9 +1,9 @@
-import createServerClient from '@/lib/supabase/supabase-server';
 import ClassicsContainer from '@/components/classics/ClassicsContainer';
+import supabase from '@/lib/supabase/client';
 
 export default async function ClassicsPage() {
-  const supabase = createServerClient();
   const { data } = await supabase.from('allClassics').select();
+  console.log(data)
   return (
     <div>
       <ClassicsContainer classics={data} />
