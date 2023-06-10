62a99c7ed8e7edad2a8bfad5a3f64ca032ab0df2
vanillovin <vanillovin@gmail.com>
2023-06-10 15:29:05 +0900

    as type casting

--- src/app/classics/page.tsx
+++ src/app/classics/page.tsx
@@ -1,12 +1,12 @@
 import ClassicsContainer from '@/components/classics/ClassicsContainer';
 import supabase from '@/lib/supabase/client';
+import { ClassicT } from './types';
 
 export default async function ClassicsPage() {
   const { data } = await supabase.from('allClassics').select();
-  console.log(data)
   return (
     <div>
-      <ClassicsContainer classics={data} />
+      <ClassicsContainer classics={data as ClassicT[]} />
     </div>
   )
 }
