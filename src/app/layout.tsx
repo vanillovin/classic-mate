6e46879b860c9c17c1ee15747d9dfd7401cfe94b
vanillovin <vanillovin@gmail.com>
2023-07-05 13:42:55 +0900

    style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
   
   return (
     <html lang="en">
-      <body>
+      <body className='example'>
         <Providers>
           <SupabaseProvider session={session}>
             <SupabaseListener serverAccessToken={session?.access_token} />
