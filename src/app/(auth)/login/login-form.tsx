a71410a7fb843a9a3b077138a4ccd4111fde4e4c
vanillovin <vanillovin@gmail.com>
2023-08-31 14:02:31 +0900

    feat: 로그인, 회원가입 입력 필드에 패턴 유효성 검사 및 최소 최대 길이 제약 조건 추가

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -42,12 +42,15 @@ export default function LoginForm() {
         className="bg-white shadow-md bg-opacity-70 py-1 px-2 rounded-sm mb-2 placeholder:text-sm"
         autoFocus
         required
+        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
       />
       <input
         type="password"
-        placeholder="비밀번호"
         name="password"
+        placeholder="비밀번호"
         className="bg-white shadow-md bg-opacity-70 py-1 px-2 rounded-sm mb-2 placeholder:text-sm"
+        minLength={6}
+        maxLength={12}
         required
       />
       <button
@@ -56,10 +59,11 @@ export default function LoginForm() {
         className="flex items-center justify-center p-2 font-medium text-sm rounded-sm hover:opacity-70 transition-all text-white bg-warm-vintage-burnt-orange shadow-md"
       >
         이메일로 로그인
-        {isLoading && 
+        {isLoading && (
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 animate-spin ml-2">
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
-          </svg>}
+          </svg>
+        )}
       </button>
     </form>
   )
