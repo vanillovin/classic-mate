034b953eb575bb7c97f7d9cac5a2705b3590a974
vanillovin <vanillovin@gmail.com>
2023-09-08 14:58:29 +0900

    refactor: 클래식 작품 검색 결과가 없을 때와 에러 메시지 및 검색 처리 시간 표시

--- src/app/classics/ClassicsContainer.tsx
+++ src/app/classics/ClassicsContainer.tsx
@@ -12,25 +12,26 @@ import Pagination from "@/components/Pagination";
 import ClassicsSearchForm from "./ClassicsSearchForm";
 
 // const SELECTED_FIELDS = "id, title, author"; // 필요한 필드
-const ITEMS_PER_PAGE = 16; // 페이지당 아이템 수
 const TOTAL_ITEMS = 60;
+const ITEMS_PER_PAGE = 16; // 페이지당 아이템 수
 
 function ClassicsContainer() {
 	const searchParams = useSearchParams();
 	const pageParam = searchParams.get("page") ?? "1";
 	const keywordParam = searchParams.get("keyword") ?? "";
 
-	const { from, to } = getPagination(TOTAL_ITEMS, +pageParam, 16);
+	const { from, to } = getPagination(TOTAL_ITEMS, +pageParam, ITEMS_PER_PAGE);
 	const { isLoading, data, isError } = useQuery({
 		queryKey: ["classics", pageParam, keywordParam],
 		queryFn: () => getClassics(from, to, keywordParam),
 		suspense: true,
   });
 
-  if (isError || !data?.classics) {
+  if (isError || !Array.isArray(data?.classics)) {
     return (
       <div className="text-center mt-20">
         <p>요청하신 페이지가 없거나 데이터가 존재하지 않습니다.</p>
+        {data?.error && <p>에러 메시지: {data.error.message}</p>}
         <Link href="/classics" className="underline font-medium">
           클래식 목록으로 돌아가기
         </Link>
@@ -42,16 +43,33 @@ function ClassicsContainer() {
     <>
       <ClassicsSearchForm />
       {!isLoading ? (
-        <>
-          <ClassicList classics={data.classics ?? []} />
-          <div className="flex items-center justify-center mt-8">
-            <Pagination
-              pathname="/classics"
-              currentPage={+pageParam}
-              totalPages={Math.ceil((data.count ?? 0) / ITEMS_PER_PAGE)}
-            />
-			    </div>
-        </>
+        data.classics.length ? (
+          <>
+            {keywordParam && (
+              <p className="m-1 font-light text-gray-400">
+                검색결과 {data.classics.length}개 ({data.took}초)
+              </p>
+            )}
+            <ClassicList classics={data.classics ?? []} />
+            <div className="flex items-center justify-center mt-8">
+              <Pagination
+                pathname="/classics"
+                currentPage={+pageParam}
+                totalPages={Math.ceil((data.count ?? 0) / ITEMS_PER_PAGE)}
+              />
+            </div>
+          </>
+        ) : (
+          <div className="text-center mt-8">
+            <p className="m-1 font-light text-gray-400">
+              검색결과 0개 ({data.took}초)
+            </p>
+            <p>
+              <span className="font-semibold">{decodeURIComponent(keywordParam)}</span>
+              와(과) 일치하는 클래식 작품이 없습니다.
+            </p>
+          </div>
+        )
       ): (
         <LoadingSkeleton />
       )}
--- src/app/classics/getClassics.ts
+++ src/app/classics/getClassics.ts
@@ -1,20 +1,40 @@
 import supabase from "@/lib/supabase/client";
+import type { PostgrestError } from '@supabase/supabase-js';
 
 export async function getClassics(
 	from: number = 0,
   to: number = 15,
   keyword: string  = ""
-): Promise<{ classics: Classic[] | null; count: number | null }> {
+): Promise<{
+  classics: Classic[] | null;
+  count: number | null;
+  took: string;
+  error?: PostgrestError | null;
+}> {
+  const start = Date.now();
   const decodeKeword = decodeURIComponent(keyword);
-
-	const { data, count } = await supabase
-		.from("all_classics")
-    .select("*", { count: "exact" })
-    .ilike('title', `%${decodeKeword}%`)
-    .range(from, to);
   
+  const { data, error, count } = await supabase
+    .from("all_classics")
+    .select("*", { count: "exact" })
+    .ilike("title", `%${decodeKeword}%`)
+    .range(from, to);
+  const end = Date.now();
+
   return {
     classics: data || null,
     count: count || null,
+    took: ((end - start) / 1000).toFixed(2),
+    error: error || null,
   };
+  // try {
+  // } catch (err) {
+  //   console.error(err);
+  //   const endAtError = Date.now();
+  //   return { 
+  //     classics: null, 
+  //     count: null, 
+  //     took: ((endAtError - start) / 1000).toFixed(2),
+  //   };
+  // }
 }
