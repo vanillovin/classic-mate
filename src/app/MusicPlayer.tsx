e8867b2f31de2d14670e2b4a5b83d9537f63fadc
vanillovin <vanillovin@gmail.com>
2023-12-05 14:22:13 +0900

    feat: 음악 플레이어에 음량 조절 기능 추가
    - 볼륨 슬라이더를 통해 음량을 조절할 수 있는 기능을 추가
    - 음소거 버튼을 눌렀을 때 이전 볼륨 값을 저장하고, 다시 눌렀을 때 이전 볼륨 값을 복원하는 기능 추가

--- src/app/MusicPlayer.tsx
+++ src/app/MusicPlayer.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import React, { useRef, useState } from "react";
 
-import useTimeOfDay, { emojiByTimeOfDay } from "@/hooks/useTimeOfDay";
+import useTimeOfDay from "@/hooks/useTimeOfDay";
 import { getCurrentDateInfo } from "@/utils/dateUtils";
 import { monthMusics, musicPlayerBackgroundImageURLs } from "./data";
 import useCurrentTime from "@/hooks/useCurrentTime";
@@ -11,6 +11,8 @@ import useCurrentTime from "@/hooks/useCurrentTime";
 function MusicPlayer() {
 	const timeOfDay = useTimeOfDay();
 	const currentTime = useCurrentTime();
+	const [volume, setVolume] = useState(1);
+	const [prevVolume, setPrevVolume] = useState(1);
 	const [duration, setDuration] = useState(0);
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [audioCurrentTime, setAudioCurrentTime] = useState(0);
@@ -18,6 +20,19 @@ function MusicPlayer() {
 
 	const { currentYearMonth } = getCurrentDateInfo();
 	const { title, coverImgUrl, musicSrcUrl } = monthMusics[currentYearMonth];
+
+	const handleMuteAudio = () => {
+		if (audioRef.current) {
+			if (audioRef.current.volume > 0) {
+				setPrevVolume(audioRef.current.volume);
+				audioRef.current.volume = 0;
+				setVolume(0);
+			} else {
+				audioRef.current.volume = prevVolume;
+				setVolume(prevVolume * 100);
+			}
+		}
+	};
 
 	const playAudio = () => {
 		audioRef.current?.play();
@@ -138,7 +153,7 @@ function MusicPlayer() {
 						max={duration}
 						value={audioCurrentTime}
 						onInput={handleProgressChange}
-						className="bg-transparent w-full appearance-none absolute top-0 left-0 cursor-pointer"
+						className="absolute bg-transparent w-full appearance-none top-0 left-0 cursor-pointer"
 						aria-label="재생 진행률"
 					/>
 				</div>
@@ -155,15 +170,31 @@ function MusicPlayer() {
 				>
 					{title}
 				</div>
-				<div
-					className={`flex items-center justify-center sm:justify-start gap-x-1 text-sm sm:text-base leading-4 font-light mt-3 sm:-mb-6 ${textColor}`}
-				>
-					<div className="rounded-full px-2 font-semibold shadow-md bg-white text-black">
-						now
-					</div>{" "}
-					<span className="font-medium">
-						{currentTime} {emojiByTimeOfDay[timeOfDay]}
-					</span>
+
+				<div className="absolute flex items-center mt-3 sm:-mb-6 gap-x-3">
+					<div
+						className={`flex items-center justify-center sm:justify-start gap-x-1 text-sm sm:text-base leading-4 font-light ${textColor}
+                
+            `}
+					>
+						<span className="rounded-full px-2 font-semibold shadow-md bg-white text-black">
+							now
+						</span>{" "}
+						<span className="font-medium">
+							{/* {emojiByTimeOfDay[timeOfDay]} */}
+							{currentTime}
+						</span>
+					</div>
+					<VolumeControl
+						volume={volume}
+						onVolumeChange={(volume: number) => {
+							if (audioRef.current) {
+								audioRef.current.volume = volume / 100;
+							}
+							setVolume(volume);
+						}}
+						onMute={handleMuteAudio}
+					/>
 				</div>
 			</div>
 		</div>
@@ -171,3 +202,65 @@ function MusicPlayer() {
 }
 
 export default MusicPlayer;
+
+type VolumeControlProps = {
+	volume: number;
+	onVolumeChange: (volume: number) => void;
+	onMute: () => void;
+};
+
+function VolumeControl({ volume, onVolumeChange, onMute }: VolumeControlProps) {
+	const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const volume = parseFloat(event.target.value);
+		onVolumeChange(volume);
+	};
+
+	return (
+		<div className="flex items-center gap-x-1">
+			<button onClick={onMute}>
+				{volume > 0 ? (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="w-4 h-4 text-autumn-darkblue"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
+						/>
+					</svg>
+				) : (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="w-4 h-4 text-autumn-darkblue"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M17.25 9.75L19.5 12m0 0l2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6l4.72-4.72a.75.75 0 011.28.531V19.94a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.506-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.395C2.806 8.757 3.63 8.25 4.51 8.25H6.75z"
+						/>
+					</svg>
+				)}
+			</button>
+			<input
+				type="range"
+				min={0}
+				max={100}
+				step={1}
+				value={volume}
+				onChange={handleVolumeChange}
+				aria-label="음량 조절"
+				className="vol-range"
+			/>
+			{/* <span className='text-xs sm:text-sm'>{volume}</span> */}
+		</div>
+	);
+}
